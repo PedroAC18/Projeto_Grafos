e53ee8c96c56c1a8829ef772293c49475ad7890a
PedroAC18 <pedro.correa1101@gmail.com>
2023-05-17 15:45:20 -0300

    Criação das entidades Vértice, Aresta e Grafo

---
+++ src/entities/Vertice.py
@@ -0,0 +1,17 @@
+class Vertice():
+    def __init__(self, id):
+        self.id = id;
+        self.visitado = False
+        self.predecessor = []
+
+    def setVisitado(self, valor):
+        self.visitado = valor
+
+    def getVisitado(self):
+        return self.visitado
+
+    def setId(self, id):
+        self.id = id
+
+    def getId(self):
+        return self.id
