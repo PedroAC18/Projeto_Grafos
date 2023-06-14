fadb42483c7175437942a52889d0a6051f8d1bfe
PedroAC18 <pedro.correa1101@gmail.com>
2023-06-14 18:21:45 -0300

    Implementação de mais métodos no grafo

--- src/entities/Graph.py
+++ src/entities/Graph.py
@@ -1,3 +1,5 @@
+import copy
+
 from src.entities.Aresta import Aresta
 from src.entities.Vertice import Vertice
 
@@ -9,8 +11,18 @@ class Graph:
         self.direcionado = direcionado
 
     def novo_Vertice(self, identificador):
-        #string = input(str("Identificador do Vertice: "))
         self.lista_Vertices.append(Vertice(identificador))
+
+    def nova_Aresta(self, origem, destino, peso):  # Método recebe dois identificadores
+        origem_aux = self.busca_Vertice(origem)
+        destino_aux = self.busca_Vertice(destino)
+        if (origem_aux is not None) and (destino_aux is not None):
+            self.lista_Arestas.append(Aresta(origem_aux, destino_aux, peso))
+        else:
+            print("Um do Vertice ou ambos são invalidos")
+
+        if self.direcionado == False:
+            self.lista_Arestas.append(Aresta(destino_aux, origem_aux, peso))  # Aresta(u,v) e Aresta(v,u)
 
     def busca_Aresta(self, u, v):  # Método recebe dois objetos do tipo Vértice
         for w in self.lista_Arestas:
@@ -26,22 +38,23 @@ class Graph:
         else:
             return None
 
-    def nova_Aresta(self, origem, destino, peso):  # Método recebe dois identificadores
-        origem_aux = self.busca_Vertice(origem)
-        destino_aux = self.busca_Vertice(destino)
-        if (origem_aux is not None) and (destino_aux is not None):
-            self.lista_Arestas.append(Aresta(origem_aux, destino_aux, peso))
-        else:
-            print("Um do Vertice ou ambos são invalidos")
-
-        if self.direcionado == False:
-            self.lista_Arestas.append(Aresta(destino_aux, origem_aux, peso))  # Aresta(u,v) e Aresta(v,u)
-
     def esta_Vazio(self):
         if len(self.lista_Vertices) == 0:
             return True
         else:
             return False
+
+
+    def copy(self):#faz uma copia do grafo para ser usado como auxiliar
+        novoGrafo = copy.deepcopy(self)
+        return novoGrafo
+    def remove_Vertice(self, v : int):
+        removido = self.copy()
+        removido.lista_Vertices[v] = []
+        for vertice in removido.lista_Vertices:
+            while v in vertice:
+                vertice.remove(v)
+        return removido
 
     def busca_Adjacente(self, u):  # Método recebe um vertice
         for i in range(len(self.lista_Arestas)):
@@ -52,3 +65,14 @@ class Graph:
                 return destino
         else:
             return None
+
+
+    def busca_predecessor(self, n:int):
+        predecessores = []
+        for vertex in self.lista_Vertices:
+            for aux in vertex:
+                if aux == n:
+                    predecessores.append(self.lista_Vertices.index(vertex))
+
+        return predecessores
+
